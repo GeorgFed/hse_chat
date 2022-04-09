59817dfab1319c5aa5520f81135fdf4b1042f148
Egor <georgefed@yandex-team.ru>
2022-04-09 22:47:35 +0300

    adds text theme and auth page

--- lib/features/auth/page.dart
+++ lib/features/auth/page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -14,28 +13,61 @@ class AuthPage extends ConsumerWidget {
           title: SvgPicture.asset(AppAssets.logo),
         ),
         body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Wrap(
-            spacing: 16.0,
-            alignment: WrapAlignment.center,
-            children: [
-              const Text('Вход'),
-              const TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Почта',
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Center(
+            heightFactor: 3.0,
+            child: Wrap(
+              spacing: 16.0,
+              runSpacing: 16.0,
+              children: [
+                Text(
+                  'Вход',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline3
+                      ?.copyWith(color: Colors.black),
+                  textAlign: TextAlign.start,
                 ),
-              ),
-              const Text(
-                'Введите адрес своей корпоративной почты, чтобы на нее пришла ссылка для входа :)',
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(elevation: 2),
-                onPressed: () {},
-                child: const Text('Войти'),
-              )
-            ],
+                const TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      borderSide: BorderSide.none,
+                    ),
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: 'Почта',
+                  ),
+                ),
+                Text(
+                  'Введите адрес своей корпоративной почты, чтобы на нее пришла ссылка для входа :)',
+                  style: Theme.of(context)
+                      .textTheme
+                      .subtitle1
+                      ?.copyWith(color: Colors.black54),
+                  textAlign: TextAlign.center,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(56.0),
+                    elevation: 0.0,
+                    primary: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: Text(
+                    'Войти',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context)
+                        .textTheme
+                        .button
+                        ?.copyWith(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       );
--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,15 @@ class MyApp extends StatelessWidget {
             elevation: 0,
             backgroundColor: Color(0xFFEFEFEF),
           ),
+          fontFamily: 'Helvetica',
+          textTheme: const TextTheme(
+            headline1: TextStyle(fontSize: 34.0, fontWeight: FontWeight.bold),
+            headline2: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+            headline3: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            button: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400),
+            bodyText1: TextStyle(fontSize: 12.0, fontWeight: FontWeight.normal),
+            subtitle1: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w400),
+          ),
         ),
         home: const AuthPage(),
       );
